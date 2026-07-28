5ba073b9cc925576e00d2afb817f09623d436d40
Alan Khodov <33288503+Markiewic@users.noreply.github.com>
2026-07-28 10:50:42 +0300

    Apply rustfmt output

--- src/sdk/install.rs
+++ src/sdk/install.rs
@@ -18,8 +18,7 @@ const SEVEN_ZIP_BOOTSTRAP_URL: &str = "https://www.7-zip.org/a/7zr.exe";
 const SEVEN_ZIP_BOOTSTRAP_SHA256: &str =
     "56b8cc9f4971cef253644fafe54063ed7fdca551d4dee0f8c6baa81b855acd72";
 const SEVEN_ZIP_URL: &str = "https://www.7-zip.org/a/7z2602-x64.exe";
-const SEVEN_ZIP_SHA256: &str =
-    "6745fa76dc2ea031596d8678f6f6b99c3c1b435b4164a63485adbbc7b8d82ef0";
+const SEVEN_ZIP_SHA256: &str = "6745fa76dc2ea031596d8678f6f6b99c3c1b435b4164a63485adbbc7b8d82ef0";
 
 pub fn run(force: bool) -> Result<()> {
     if !cfg!(target_os = "windows") {
