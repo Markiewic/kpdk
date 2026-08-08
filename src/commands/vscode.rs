fd432238d64e0cf807e388550d3fe3866b9ca294
Alan Khodov <33288503+Markiewic@users.noreply.github.com>
2026-08-08 21:19:36 +0300

    Fix JSON format string escaping

--- src/commands/vscode.rs
+++ src/commands/vscode.rs
@@ -71,7 +71,7 @@ fn cpp_properties(config: &ProjectFile) -> String {
     .collect::<Vec<_>>()
     .join(",\n");
 
-    format!("{\n  \"configurations\": [\n{configurations}\n  ],\n  \"version\": 4\n}\n")
+    format!("{{\n  \"configurations\": [\n{configurations}\n  ],\n  \"version\": 4\n}}\n")
 }
 
 fn cpp_configuration(config: &ProjectFile, name: &str, include: &str) -> String {
